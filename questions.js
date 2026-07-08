488dc41896a3798a33903ac32adf140815696023
Engineer Rocky <yadavrintu06@gmail.com>
2026-07-09 01:13:32 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,18 @@
+const examData = {
+    examName: "Soil Mechanics - Master Test",
+    timeInMinutes: 30,
+    questions: [
+        { 
+            q: "What is the primary purpose of Soil Compaction?", 
+            opt: ["Remove Air", "Remove Water", "Increase Weight", "Increase Strength"], 
+            ans: 0, 
+            sol: "Compaction is the process of densification of soil by reducing air voids. It increases soil strength and reduces permeability." 
+        },
+        { 
+            q: "Void ratio is defined as?", 
+            opt: ["Vv/Vs", "Vv/V", "Vs/Vv", "None"], 
+            ans: 0, 
+            sol: "Void ratio (e) is the ratio of the volume of voids (Vv) to the volume of soil solids (Vs)." 
+        }
+    ]
+};
